e45ded8788d5f99abb0a015eead3c838ff6fd3d2
Roberto Fernandez <roberto.fernandez@willdom.com>
2023-08-26 22:54:21 -0300

    #4 Map building from text representation: Example level base and walls rendering.

--- Shutdown Mission/Assets/Scripts/com/sdmission/view/MapsManager.cs
+++ Shutdown Mission/Assets/Scripts/com/sdmission/view/MapsManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
 
 namespace com.sdmission.view
 {
@@ -18,18 +19,55 @@ namespace com.sdmission.view
 		void Start()
         {
             Debug.Log("View started");
+
+			string filePath = Application.dataPath + "/Levels/level1.txt";
+			Debug.Log("reading " + filePath);
 			
+			float currentX = 0f;
+			float currentZ = 0f;
 			
-			// remove example
-			Vector3 position = new Vector3(0f, 0f, 0f);
-			Quaternion rotation = Quaternion.identity;
-			GameObject instantiatedPrefab = Instantiate(wallTile, position, rotation);
-			
+			if (File.Exists(filePath))
+			{
+				string[] lines = File.ReadAllLines(filePath);
+				foreach (string line in lines)
+				{
+					currentX = 0f;
+					//Debug.Log(line);
+					for (int i = 0; i < line.Length; i++)
+					{
+						char c = line[i];
+						if('1' == c) {
+							instantiateWall(currentX, currentZ);
+						} else if('0' == c) {
+							instantiateBase(currentX, currentZ);
+						}
+						currentX++;
+					}
+					currentZ++;
+				}
+			}
+			else
+			{
+				Debug.LogError("No level file.");
+			}
+    			
 		}
 		
 		private void Update()
         {
 		
-		}		
+		}
+		
+		public void instantiateWall(float x, float z) {
+			Vector3 position = new Vector3(x, 0f, z);
+			Quaternion rotation = Quaternion.identity;
+			GameObject instantiatedPrefab = Instantiate(wallTile, position, rotation);
+		}
+		
+		public void instantiateBase(float x, float z) {
+			Vector3 position = new Vector3(x, -0.5f, z);
+			Quaternion rotation = Quaternion.identity;
+			GameObject instantiatedPrefab = Instantiate(baseTile, position, rotation);
+		}
     }
 }
